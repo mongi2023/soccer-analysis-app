a0caaa8ff4667fb93c274da1db316dc814f6c5bb
hicham <hicham.hamza.mounadi@gmail.com>
2022-08-03 15:25:59 +0100

    team: updating team controllers

--- back-end/team/teamControllers.js
+++ back-end/team/teamControllers.js
@@ -1,54 +1,148 @@
-const {StatusCodes} = require('http-status-codes')
-const CustomError = require('../shared-services/errors')
+const { StatusCodes } = require('http-status-codes');
+const { getPlayersTeamService } = require('../player/playerServices');
+const { getProjectByIdService } = require('../project/projectServices');
+const CustomError = require('../shared-services/errors');
 const {
-createTeamService,
-    getTeamByIdService,
-    getTeamsService,
-    updateTeamService
-} = require('./teamServices')
+  createTeamService,
+  getTeamByIdService,
+  getTeamsService,
+  updateTeamService,
+  getTeamByNameService,
+} = require('./teamServices');
 
+/**
+ * ? Create a team controller
+ * @param {*} req
+ * @param {*} res
+ */
+const createTeamController = async (req, res) => {
+  const userId = req.user.userId;
+  const { name, creation_date, logo, project } = req.body;
+  if (!name || !creation_date || !logo || !project) {
+    throw new CustomError.BadRequestError(`All team's data are mandatory`);
+  }
 
-const createTeamController = async(req,res) => {
-    const {name,
-           creation_date,
-           logo} = req.body
-    if(name === '' || creation_date === '' || logo === ''){
-        throw new CustomError.BadRequestError(`All team's data are mandatory`)
+  // ! Check if the project belongs to the connected user
+  const projectBelongsToTheConnectedUser = await getProjectByIdService(
+    project,
+    userId
+  );
+  if (!projectBelongsToTheConnectedUser) {
+    throw new CustomError.BadRequestError('This project does not exist');
+  }
+  const isTeamExist = await getTeamByNameService(name, userId);
+  console.log(isTeamExist);
+  // const userId = req.user.userId
+  // ! Team exist within the DB
+  if (isTeamExist) {
+    throw new CustomError.BadRequestError(
+      'This team is already created, please choose another name'
+    );
+  }
+
+  // ! Team exist within the PROJECT
+  if (isTeamExist !== null) {
+    if (isTeamExist.project.equals(project)) {
+      throw new CustomError.BadRequestError(
+        'This team is already created within this Project'
+      );
     }
-    const team = await createTeamService({...req.body})
-    res.status(StatusCodes).send({msg: `Team added successfully`})
-}
+  }
 
-const getTeamsController = async(req, res) => {
-    const teams = await getTeamsService()
-    res.status(StatusCodes.OK).send({teams: teams})
-}
+  req.body.user = req.user.userId;
+  const team = await createTeamService({ ...req.body });
+  res.status(StatusCodes.CREATED).send({ msg: `Team added successfully` });
+};
 
-const getTeamByIdController = async(req, res) => {
-    const team_id = req.body.id
-    const team = await getTeamByIdService(team_id)
-    if(!team){
-        throw new CustomError.NotFoundError(`There is no team with the id : ${team_id}`)
-    }
-    res.status(StatusCodes.OK).send({team: team})
-}
+/**
+ * ? Get all the teams within a specific project, created
+ * ? by the connected user
+ * @param {*} req
+ * @param {*} res
+ */
+const getTeamsController = async (req, res) => {
+  const project = req.body.project;
+  const userId = req.user.userId;
 
-const updateTeamController = async(req, res) => {
-    const {
-        params: {id: team_id},
-        body: {name, creation_date,logo}
-    } = req
-    if(name === '' || creation_date === '' || logo === ''){
-        throw new CustomError.BadRequestError(`All fields needs to be filled`)
-    }
-    const team = await updateTeamService(team_id, req.body)
-    res.status(StatusCodes.OK).send({msg: `Team updated successfully`})
-}
+  const doesProjectExist = await getProjectByIdService(project, userId);
+  if (!doesProjectExist) {
+    throw new CustomError.NotFoundError('The project does not exist');
+  }
+  const teams = await getTeamsService(project, userId);
+  //  ! TODO : NEEDS A CORRECTION IMMEDIATELY
+  //   if(!teams[0].user.equals(userId)){
+  //     throw new CustomError.NotFoundError('No team')
+  //   }
+  res.status(StatusCodes.OK).send({ teams: teams });
+};
 
+const getTeamByIdController = async (req, res) => {
+  const team_id = req.params.id;
+  const userId = req.user.userId;
+  if (!userId) {
+    throw new CustomError.UnauthenticatedError('You are not authorized!');
+  }
+  const team = await getTeamByIdService(team_id, userId);
+  if (!team) {
+    throw new CustomError.NotFoundError(
+      `There is no team with the id : ${team_id}`
+    );
+  }
+  res.status(StatusCodes.OK).send({ team: team });
+};
+
+const updateTeamController = async (req, res) => {
+  const {
+    params: { id: team_id },
+    user: { userId },
+    body: { name, creation_date, logo, project },
+  } = req;
+  if (!userId) {
+    throw new CustomError.UnauthenticatedError('You are not authorized!');
+  }
+  if (!name || !creation_date || !logo || !project) {
+    throw new CustomError.BadRequestError(`All fields needs to be filled`);
+  }
+  const find_team_using_id = await getTeamByIdService(team_id, userId);
+  if (!find_team_using_id) {
+    throw new CustomError.NotFoundError('No team with the this id');
+  }
+
+  // * Find the teams with the same name except for this current team
+  const isTeamExist = await getTeamByNameService(name, userId)
+    .where('_id')
+    .ne(team_id);
+  console.log(isTeamExist);
+  if (isTeamExist) {
+    throw new CustomError.BadRequestError('This team is already exist');
+  }
+  const team = await updateTeamService(team_id, req.body);
+  if (!team) {
+    throw new CustomError.NotFoundError('There is no team with this id');
+  }
+  res.status(StatusCodes.OK).send({ msg: `Team updated successfully` });
+};
+
+const getPlayersOfTeamController = async(req, res) => {
+  const {
+    params: {id: team_id},
+    user: {userId}
+  } = req
+  console.log(team_id, userId)
+  const doesTeamBelongsToConnctedUser = await getTeamByIdService(team_id, userId)
+  console.log("TEAM : ",doesTeamBelongsToConnctedUser)
+  if(!doesTeamBelongsToConnctedUser){
+    throw new CustomError.NotFoundError('This team does not exist')
+  }
+  const players = await getPlayersTeamService(team_id, userId)
+
+  res.status(StatusCodes.OK).send({players: players})
+}
 
 module.exports = {
-    createTeamController,
-    getTeamByIdController,
-    getTeamsController,
-    updateTeamController
-}
+  createTeamController,
+  getTeamByIdController,
+  getTeamsController,
+  updateTeamController,
+  getPlayersOfTeamController
+};
