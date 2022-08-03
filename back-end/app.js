9df776f1630105b770bdf570cdef477820683e03
hicham <hicham.hamza.mounadi@gmail.com>
2022-08-03 16:55:38 +0100

    adding cookie-parser to app.js

--- back-end/app.js
+++ back-end/app.js
@@ -5,6 +5,7 @@ const app = express();
 const helmet = require('helmet')
 const xss = require('xss-clean')
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 // * ROUTES IMPORTS
 const projectRouter = require('./project/projectRoutes');
