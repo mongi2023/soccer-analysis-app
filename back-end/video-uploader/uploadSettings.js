9446c041cfe1d7cc1ccc28acf58d4290d0612794
hicham <hicham.hamza.mounadi@gmail.com>
2022-08-03 15:37:44 +0100

    video: adding setting file for uploading video

---
+++ back-end/video-uploader/uploadSettings.js
@@ -0,0 +1,21 @@
+multer = require("multer");
+
+// File upload settings
+const PATH = './uploads';
+  let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, PATH);
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+
+  let upload = multer({
+    storage: storage,
+  });
+
+
+module.exports = {
+upload
+}
