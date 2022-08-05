0310ecbda34b14821f249e8f2270628052438c0b
mongi22 <mongi.thabet@tekniva.net>
2022-08-05 11:25:11 +0100

    login service

--- front-end/src/app/core/auth/login/login.component.ts
+++ front-end/src/app/core/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+ user!:User
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
   }
+loginController(){
+this.authService.loginService(this.user).subscribe(data=>{
+  console.log(data);
+  
+},(error)=>{
+  console.log(error);
+  
+})
+}
 
 }
--- front-end/src/app/core/interceptor/auth.interceptor.ts
+++ front-end/src/app/core/interceptor/auth.interceptor.ts
@@ -13,6 +13,30 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
-  }
-}
+    request = request.clone({
+      withCredentials:true
+    })
+      request = request.clone({
+        headers: request.headers.set('Content-Type', 'application/json')
+      });
+      request = request.clone({
+        headers: request.headers.set('accept', 'application/json')
+      });
+    // if (token) {
+    //   request = request.clone({
+    //     headers: request.headers.set('Authorization', 'Bearer ' + token)
+    //   });
+    //   return next.handle(request).pipe(
+    //     catchError((error: HttpErrorResponse) => {
+    //       if (error && error.status === 401) {
+    //         console.log("error unauthorized");
+    //       }
+    //       const err = error.error.message || error.statusText;
+    //       return throwError(error)
+    //     })
+    //   )
+    // }
+    // else{
+      return next.handle(request)
+    // }
+  }}
